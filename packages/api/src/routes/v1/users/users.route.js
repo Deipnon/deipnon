421c8bbe3947999bc808d851c3432e123610b5ca
Emre DEĞER <ben@emre.dev>
2019-10-05 01:51:19 +0300

    fix(users.route): changed user routes.

--- packages/api/src/routes/v1/users/users.route.js
+++ packages/api/src/routes/v1/users/users.route.js
@@ -1,22 +1,23 @@
 import { Router } from 'express'
-import { sampleMiddleware } from '../../../middlewares/v1/'
-
 import userService from '../../../services/v1/users/users.service'
 
 const router = Router()
 
 /*
-router.post('/hello', sampleMiddleware, async (req, res) => {
+	Ornek middleware kullanimi.
+	router.post('/example', sampleMiddleware, async (req, res) => ...
+*/
+
+router.post('/user', async (req, res) => {
   const user = await userService.createUser(req.body)
-  if (!userService.status) return res.status(400).send(user)
+  if (!user.status) return res.status(400).send(user)
   return res.status(201).send(user)
 })
 
-router.get('/hello', sampleMiddleware, async (req, res) => {
+router.get('/user', async (req, res) => {
   const user = await userService.getUser(req.body)
-  if (!userService.status) return res.status(400).send(user)
+  if (!user.status) return res.status(400).send(user)
   return res.status(201).send(user)
 })
-*/
 
 export default router
