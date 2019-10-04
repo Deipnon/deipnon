25c955f8b4735be2f682e2235d7912f27d523456
Emre DEĞER <ben@emre.dev>
2019-10-04 22:50:15 +0300

    fix(users.route.js): sample routes commented

--- packages/api/src/routes/v1/users/users.route.js
+++ packages/api/src/routes/v1/users/users.route.js
@@ -5,15 +5,18 @@ import userService from '../../../services/v1/users/users.service'
 
 const router = Router()
 
+/*
 router.post('/hello', sampleMiddleware, async (req, res) => {
   const user = await userService.createUser(req.body)
   if (!userService.status) return res.status(400).send(user)
   return res.status(201).send(user)
 })
+
 router.get('/hello', sampleMiddleware, async (req, res) => {
   const user = await userService.getUser(req.body)
   if (!userService.status) return res.status(400).send(user)
   return res.status(201).send(user)
 })
+*/
 
 export default router
