08a531dceef25937c65f63cf22bd0576126536eb
Gizem Selli <gizem.selli@matriksdata.com>
2019-10-06 15:16:29 +0200

    Button elementine color, button type ve block  property'leri eklendi. Storybook'ta görünümleri oluşturuldu.

--- packages/components/src/Button/index.js
+++ packages/components/src/Button/index.js
@@ -1,12 +1,58 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
+import { css } from '@emotion/core'
 import theme from '@deipnon/themes/lib'
 
-const ButtonElement = styled.button`
-  background-color: ${theme.colors.primaryButtonBg};
-  color: ${theme.colors.primaryButton};
+// const ButtonElement = styled.button`
+//   background-color: ${theme.colors.primaryButtonBg};
+//   color: ${theme.colors.primaryButton};
+// `
+
+
+const buttonStyles = ({ color }) => {
+    if (color === 'primary') {
+        return css`
+        color: #fff;
+        background-color: #007bff;
+        border-color: #007bff;
+    `
+    }
+    if (color === 'secondary') {
+        return css`
+        color: #fff;
+        background-color: #6c757d;
+        border-color: #6c757d;
+    `
+    }
+}
+
+const withBlock = ({ block }) => block && css`
+    width: 100%
 `
 
-const Button = ({ children }) => <ButtonElement>{children}</ButtonElement>
+const ButtonElement = styled.button`
+    display: inline-block;
+    font-weight: 400;
+    color: #212529;
+    text-align: center;
+    vertical-align: middle;
+    user-select: none;
+    background-color: transparent;
+    border: 1px solid transparent;
+    padding: .375rem .75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: .25rem;
+    cursor: pointer;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    ${buttonStyles}
+    ${withBlock}
+`
+const props = {
+    children: Element
+}
+const Button = props => <ButtonElement {...props}>{props.children}</ButtonElement>
+
+// const Button = ({ children, color, block, type }) => <ButtonElement color={color} block={block} type={type}>{children}</ButtonElement>
 
 export default Button
