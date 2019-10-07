451ac14fe96525d29cffdc0ef6a200fc1d3abae7
Melih Korkmaz <melih@findhotel.net>
2019-10-07 10:24:36 +0200

    select list componenti + travis (#29)

--- packages/themes/src/defaultTheme/styles.js
+++ packages/themes/src/defaultTheme/styles.js
@@ -4,17 +4,18 @@ import colors from './colors'
 
 export default {
 
-	// Input styles
-	inputFontSize: fonts.size.md,
-	inputFontWeight: fonts.weight.sm,
-	inputLineHeight: fonts.lineHeight.md,
-	inputColor: colors.darkGray,
-	inputBgColor: colors.white,
-	inputBorderColor: colors.lightGrayishBlue,
-	inputBorderWidth: '1px',
-	inputBorderRadius: `${layout.radius.sm}px`,
-	inputErrorBorderColor: colors.red,
-	inputPaddings: `${layout.basePadding * 0.75}px ${layout.basePadding * 1.5}px`,
+	// Form styles
+	formFontSize: fonts.size.md,
+	formFontWeight: fonts.weight.sm,
+	formLineHeight: fonts.lineHeight.md,
+	formColor: colors.darkGray,
+	formBgColor: colors.white,
+	formBorderColor: colors.lightGrayishBlue,
+	formBorderWidth: '1px',
+	formBorderRadius: `${layout.radius.sm}px`,
+	formErrorBorderColor: colors.red,
+	formPaddings: `${layout.basePadding * 0.75}px ${layout.basePadding * 1.5}px`,
+	formHeight: `${layout.basePadding * 4.5}px`,
 
 	// Buttons
 	primaryButtonBg: colors.blue,
