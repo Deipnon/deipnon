01a70c3d5edef96fa5549870b8dd177ef9ff2a07
Mert Koseoglu <info@mkoseoglu.com>
2019-10-07 20:33:49 +0300

    delete index.js

--- packages/api/src/routes/index.js
+++ packages/api/src/routes/index.js
@@ -1,9 +1,9 @@
 import { VERSION } from '../config'
-import userRoute from './v1/users/users.route'
+import userRoute from './users/users.route'
 
 export class Routes {
-  routes (app) {
-    app.use(VERSION + '/user', userRoute)
-    // app.use(VERSION + '/foo', fooRoute)
-  }
+	routes (app) {
+		app.use(VERSION + '/user', userRoute)
+		// app.use(VERSION + '/foo', fooRoute)
+	}
 }
--- packages/api/src/routes/users/users.route.js
+++ packages/api/src/routes/users/users.route.js
@@ -1,18 +1,18 @@
 import { Router } from 'express'
-import userService from '../../../services/users/users.service'
+import userService from '../../services/users/users.service'
 
 const router = Router()
 
 router.post('/', async (req, res) => {
-  const user = await userService.createUser(req.body)
-  if (!user.status) return res.status(400).send(user)
-  return res.status(201).send(user)
+	const user = await userService.createUser(req.body)
+	if (!user.status) return res.status(400).send(user)
+	return res.status(201).send(user)
 })
 
 router.get('/', async (req, res) => {
-  const user = await userService.getUser(req.body)
-  if (!user.status) return res.status(400).send(user)
-  return res.status(201).send(user)
+	const user = await userService.getUser(req.body)
+	if (!user.status) return res.status(400).send(user)
+	return res.status(201).send(user)
 })
 
 export default router
