cdb3ccb2ef84f11f827041e373062dc01c2935ce
Emre DEĞER <emredeger16@gmail.com>
2019-10-08 22:08:55 +0300

    fix(user): fixed route and added route test.

--- packages/api/src/routes/users/users.route.js
+++ packages/api/src/routes/users/users.route.js
@@ -1,18 +1,45 @@
 import { Router } from 'express'
-import userService from '../../services/users/users.service'
+import User from '../../models/Users'
 
 const router = Router()
 
-router.post('/', async (req, res) => {
-	const user = await userService.createUser(req.body)
-	if (!user.status) return res.status(400).send(user)
-	return res.status(201).send(user)
+router.post('/', (req, res) => {
+	const { firstName, lastName, email } = req.body
+
+	User.create({
+		firstName,
+		lastName,
+		email
+	})
+		.then(user => {
+			res.json({
+				status: true,
+				data: user
+			})
+		})
+		.catch(() => {
+			res.status(500).json({
+				status: false
+			})
+		})
 })
 
-router.get('/', async (req, res) => {
-	const user = await userService.getUser(req.body)
-	if (!user.status) return res.status(400).send(user)
-	return res.status(201).send(user)
+router.get('/:id', (req, res) => {
+	const { id } = req.params
+
+	User.findById(id)
+		.then(user => {
+			res.json({
+				status: true,
+				data: user
+			})
+		})
+		.catch(() => {
+			res.status(500).json({
+				status: false,
+				data: null
+			})
+		})
 })
 
 export default router
