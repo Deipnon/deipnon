cbca8d69dc9dc5937d67b45ee37e445d8f964473
Melih Korkmaz <melih@findhotel.net>
2019-10-25 17:34:16 +0200

    Added grids (#49)

--- packages/themes/src/defaultTheme/index.js
+++ packages/themes/src/defaultTheme/index.js
@@ -1,9 +1,11 @@
 import colors from './colors'
 import fonts from './fonts'
 import styles from './styles'
+import gridStyles from './styles/gridStyles'
 
 export default {
 	colors,
 	fonts,
-	styles
+	styles,
+	gridStyles
 }
