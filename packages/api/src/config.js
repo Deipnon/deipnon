42b5b4389ba3c87a9eee80ea040d4d01a902b10d
Emre DEĞER <emredeger16@gmail.com>
2019-10-25 03:03:05 +0300

    chore(api): added jwt secret on env.

--- packages/api/src/config.js
+++ packages/api/src/config.js
@@ -2,5 +2,5 @@ import dotEnv from 'dotenv'
 dotEnv.config()
 
 export const {
-	env: { PORT, VERSION, DB_CONNECTION, IS_DEV }
+	env: { PORT, VERSION, DB_CONNECTION, IS_DEV, JWT_SECRET }
 } = process
