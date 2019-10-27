fcc257c85a9931785fa59de4b0aff2709539b662
melihkorkmaz <melih@findhotel.net>
2019-10-27 18:03:39 +0100

    Remove dashboard test codes

--- packages/dashboard/src/views/Dashboard.js
+++ packages/dashboard/src/views/Dashboard.js
@@ -1,31 +1,7 @@
 // @flow
 import * as React from 'react'
-import gql from 'graphql-tag'
-import { useQuery } from '@apollo/react-hooks'
-
-const GET_RESTAURANT = gql`
-	{
-		restaurant(id: "5dad739a2525263ef51bb407") {
-			_id
-			name
-			phone
-			email
-			address {
-				addressLine
-				zipCode
-				city
-				state
-			}
-		}
-	}
-`
 
 const Dashboard = () => {
-	const { loading, error } = useQuery(GET_RESTAURANT)
-
-	if (loading) return 'Loading...'
-	if (error) return `Error! ${error.message}`
-
 	return (<React.Fragment>
 		Dashboard
 	</React.Fragment>)
